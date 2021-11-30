43185483d3617de10048d3de98c1f8044e1ea8ac
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2021-11-30 15:31:46 +0200

    Update substitude.py

--- 4.Other Exercises/substitude.py
+++ 4.Other Exercises/substitude.py
@@ -1,14 +1,20 @@
-k - int(input())
+k = int(input())
 l = int(input())
-m - int(input())
-n - int(input())
-
-for number_1 in range(k, 8+1):
-     for number_2 in range(9, 1-1, -1):
-           for number_3 in range(m, 8+1):
-                for number_4 in range(9, n-1, -1):
-                     if (number_1 % 2 == 0 and not number_2 % 2 == 0) and (number_3 % 2 == 0 and not number_4 % 2 == 0):
-                          if not number_1 == number_3 and not number_2 == number_4:
-                              print(f"{number_1}{number_2} - {number_3}{number_4}")
-                          else:
-                               print("Cannot change the same player.")
+m = int(input())
+n = int(input())
+counter = 0
+for symbol_1 in range(k, 9):
+    if symbol_1 % 2 == 0:
+        for symbol_2 in range(9, (l - 1), - 1):
+            if symbol_2 % 2 != 0:
+                for symbol_3 in range(m, 9):
+                    if symbol_3 % 2 == 0:
+                        for symbol_4 in range(9, (n - 1), - 1):
+                            if symbol_4 % 2 != 0:
+                                if counter == 6:
+                                    break
+                                if f'{symbol_1}{symbol_2}' == f'{symbol_3}{symbol_4}':
+                                    print('Cannot change the same player.')
+                                else:
+                                    print(f'{symbol_1}{symbol_2} - {symbol_3}{symbol_4}')
+                                    counter += 1
