2efa15cdc3ff400ec0ea0c41270a0f314b9adbf8
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2021-12-01 15:02:54 +0200

    Delete 1.Exercises/10.While Loop - Exercise/More Exercises directory

--- 1.Exercises/10.While Loop - Exercise/More Exercises/average_number.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-average_sum = 0
-
-for i in range(n):
-    number = int(input())
-    average_sum += number / n
-
-print(f"{average_sum:.2f}")
--- 1.Exercises/10.While Loop - Exercise/More Exercises/numbers_divided_by_3_without_reminder.py
+++
@@ -1,3 +0,0 @@
-for i in range(3, 100):
-    if i % 3 == 0:
-        print(i)
