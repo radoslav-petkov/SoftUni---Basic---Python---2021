9869c50b495db7045c19a5c77aa312e45314d6a8
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2021-12-03 17:30:20 +0200

    Update travelling.py

--- 1.Exercises/11.Nested Loops - Lab/travelling.py
+++ 1.Exercises/11.Nested Loops - Lab/travelling.py
@@ -1,17 +1,35 @@
-available_sum = 0
+destination = input()
 
-while True:
+while destination != 'End':
+    min_budget = float (input ())
+    savings = 0
+
+    while savings < min_budget:
+        # Earning money till Ani has enough savings to start the trip
+        curr_money_earned = float (input ())
+        savings += curr_money_earned
+    # Ani has enough savings in order to start the trip
+    print(f'Going to {destination}!')
+
     destination = input()
-    if destination == "End":
-        break
-    needed_money = float(input())
 
-    while available_sum < needed_money:
-        current_sum = float(input())
-        available_sum += current_sum
+# or
 
-    print(f"Going to {destination}!")
-    available_sum = 0
+
+# available_sum = 0
+
+# while True:
+#     destination = input()
+#     if destination == "End":
+#         break
+#     needed_money = float(input())
+
+#     while available_sum < needed_money:
+#         current_sum = float(input())
+#         available_sum += current_sum
+
+#     print(f"Going to {destination}!")
+#     available_sum = 0
 
 
 
