2fdcdf1136cc669ef51bd145699a2344e8b108d0
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2021-12-05 13:05:07 +0200

    Delete 1.Exercises/12.Nested Loops - Exercise directory

--- 1.Exercises/12.Nested Loops - Exercise/special_numbers.py
+++
@@ -1,9 +0,0 @@
-special_number = int(input())
-
-
-for first_num in range(1, 10):
-    for second_num in range(1, 10):
-        for third_num in range(1, 10):
-            for fourth_num in range(1,10):
-                if special_number % first_num == 0 and special_number % second_num == 0 and special_number % third_num == 0 and special_number % fourth_num == 0:
-                    print(f"{first_num}{second_num}{third_num}{fourth_num}", end=" ")
