f59d0f2814f45a23430f1fcd12113cd588319c2f
Radoslav Petkov <93141081+radoslav-petkov@users.noreply.github.com>
2021-12-14 21:22:39 +0200

    Delete Preparation_exam directory

--- Preparation_exam/oscars_ceremony.py
+++
@@ -1,9 +0,0 @@
-hall_rent = int(input ())
-
-statues_price = hall_rent - (hall_rent * 0.3)
-catering_price = statues_price - (statues_price * 0.15)
-sound_price = catering_price * 0.5
-
-total_price = hall_rent + statues_price + catering_price + sound_price
-
-print(f'{total_price:.2f}')
